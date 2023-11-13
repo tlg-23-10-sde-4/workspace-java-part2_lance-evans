91e7eb857ec0466ef542ba1a426ce19644123f0b
Lance Evans <lance.evans@tlgcohort.com>
2023-11-13 09:07:27 -0700

    Department uses .forEach(Consumer) instead of for-each loops

--- Lab03.2/src/com/javatunes/personnel/Department.java
+++ Lab03.2/src/com/javatunes/personnel/Department.java
@@ -25,11 +25,11 @@ public class Department {
   }
   
   public void listEmployees() {
-    employees.forEach(emp -> System.out.println(emp));
+    employees.forEach(System.out::println); // emp -> System.out.println(emp)
   }
   
   public void workEmployees() {
-    employees.forEach(emp -> emp.work());
+    employees.forEach(Employee::work); // emp -> emp.work()
   }
   
   /**
@@ -37,7 +37,7 @@ public class Department {
    * it will look similar to the workEmployees() method above
    */
   public void payEmployees() {
-    employees.forEach(emp -> emp.pay());
+    employees.forEach(Employee::pay); // emp -> emp.pay()
   }
   
   // helper method to add an Employee to the collection
