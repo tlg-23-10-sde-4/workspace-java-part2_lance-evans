f9c41849169a8403f614b6f96b422826e8010c02
Lance Evans <lance.evans@tlgcohort.com>
2023-11-13 13:09:46 -0700

    Lab11.1B implements Runnable

--- Lab11.1B/src/com/javatunes/thread/MessagePrinter.java
+++ Lab11.1B/src/com/javatunes/thread/MessagePrinter.java
@@ -7,40 +7,51 @@
  */
 package com.javatunes.thread;
 
-// DONE: extend the Thread class
-public class MessagePrinter extends Thread {
-  private String message;
-  private int interval = 500;  // default value
-  
-  public MessagePrinter(String message) {
-    this.message = message;
-    // DONE: set the thread name [important when debugging]
-    setName("Message Printer");
-  }
-  
-  public MessagePrinter(String message, int interval) {
-    this(message);
-    this.interval = interval;
-  }
-  
-  /**
-   * DONE: implement run() as follows:
-   * It should loop 10 times, printing the 'message' field to stdout,
-   * then pausing for some interval (in millis) that you choose.
-   * 
-   * Use a try/catch to call Thread.sleep(interval) for the pause.
-   * The sleep() method throws InterruptedException, which you need to catch.
-   * You can either leave the catch block empty, or print the exception to stdout.
-   */
-  @Override
-  public void run() {
-    for (int i = 0; i < 10; i++) {
-      System.out.println(getName() + ": " + message);
-      try {
-        Thread.sleep(interval);
-      }
-      catch (InterruptedException ignored) {
-      }
+// TODO: extend the Thread class
+public class MessagePrinter implements Runnable {
+    private static final String threadPrefix = "message-printer";
+    private static int id = 0;
+
+    private final String message;
+    private int count;
+    private long interval;
+
+    public MessagePrinter(String message, int count, long interval) {
+        this.message = message;
+        this.count = count;
+        this.interval = interval;
+        //Thread.currentThread().setName(threadPrefix+ ++id);
+        // set the thread name [important when debugging]
     }
-  }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setInterval(long interval) {
+        this.interval = interval;
+    }
+
+    /**
+     * implement run() as follows:
+     * It should loop 10 times, printing the 'message' field to stdout,
+     * then pausing for some interval (in millis) that you choose.
+     * <p>
+     * Use a try/catch to call Thread.sleep(interval) for the pause.
+     * The sleep() method throws InterruptedException, which you need to catch.
+     * You can either leave the catch block empty, or print the exception to stdout.
+     */
+
+
+  @Override
+    public void run() {
+
+        for (int i = 0; i <= count; i++) {
+          System.out.println(Thread.currentThread().getName()+": " + message);
+        }
+        try {
+          Thread.sleep(interval);
+        }
+        catch (InterruptedException ignored){}
+    }
 }
--- Lab11.1B/src/com/javatunes/thread/MessagePrinterClient.java
+++ Lab11.1B/src/com/javatunes/thread/MessagePrinterClient.java
@@ -9,16 +9,21 @@ package com.javatunes.thread;
 
 public class MessagePrinterClient {
 
-  public static void main(String[] args) {
-    // DONE: create two instances of your thread subclass and start them up
-    MessagePrinter printer1 = new MessagePrinter("Roses are red");  // take the default interval
-    MessagePrinter printer2 = new MessagePrinter("Violets are blue", 1375);
-    
-    printer1.start();
-    printer2.start();
-    
-    // just for demonstration purposes - this shows why you don't call run() directly
-    // printer1.run();
-    // printer2.run();
-  }
+    public static void main(String[] args) {
+        MessagePrinter m1 = new MessagePrinter("Roses are red", 30, 1000);
+        MessagePrinter m2 = new MessagePrinter("Violets are blue", 30, 500);
+        MessagePrinter m3 = new MessagePrinter("I dont care", 30, 100);
+        MessagePrinter m4 = new MessagePrinter("and neither should you", 30, 2000);
+
+        Thread thd = new Thread(m1, "message-printer-1");
+        Thread thd1 = new Thread(m2, "message-printer-2");
+        Thread thd2 = new Thread(m3,"message-printer-3");
+        Thread thd3 = new Thread(m4, "message-printer-4");
+
+        thd.start();
+        thd1.start();
+        thd2.start();
+        thd3.start();
+        // create two instances of your thread subclass and start them up
+    }
 }
