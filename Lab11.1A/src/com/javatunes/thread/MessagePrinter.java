ef5ed082bdb9d96f3fb76a1bafb040f1c1d61b9a
Lance Evans <lance.evans@tlgcohort.com>
2023-11-13 11:33:16 -0700

    Lab11.1A extending Thread

--- Lab11.1A/src/com/javatunes/thread/MessagePrinter.java
+++ Lab11.1A/src/com/javatunes/thread/MessagePrinter.java
@@ -8,24 +8,48 @@
 package com.javatunes.thread;
 
 // TODO: extend the Thread class
-public class MessagePrinter {
-  private String message;
-  
-  public MessagePrinter(String message) {
-    this.message = message;
-    // TODO: set the thread name [important when debugging]
-  }
-  
-  /**
-   * TODO: implement run() as follows:
-   * It should loop 10 times, printing the 'message' field to stdout,
-   * then pausing for some interval (in millis) that you choose.
-   * 
-   * Use a try/catch to call Thread.sleep(interval) for the pause.
-   * The sleep() method throws InterruptedException, which you need to catch.
-   * You can either leave the catch block empty, or print the exception to stdout.
-   */
-  public void run() {
-    
-  }
+public class MessagePrinter extends Thread {
+    private static final String threadPrefix = "message-printer";
+    private static int id = 0;
+
+    private String message;
+    private int count;
+    private long interval;
+
+    public MessagePrinter(String message) {
+        this.message = message;
+        setName(threadPrefix+ ++id);
+        // set the thread name [important when debugging]
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setInterval(long interval) {
+        this.interval = interval;
+    }
+
+    /**
+     * implement run() as follows:
+     * It should loop 10 times, printing the 'message' field to stdout,
+     * then pausing for some interval (in millis) that you choose.
+     * <p>
+     * Use a try/catch to call Thread.sleep(interval) for the pause.
+     * The sleep() method throws InterruptedException, which you need to catch.
+     * You can either leave the catch block empty, or print the exception to stdout.
+     */
+
+
+  @Override
+    public void run() {
+
+        for (int i = 0; i <= count; i++) {
+          System.out.println(getName()+": " + message);
+        }
+        try {
+          Thread.sleep(interval);
+        }
+        catch (InterruptedException ignored){}
+    }
 }
--- Lab11.1A/src/com/javatunes/thread/MessagePrinterClient.java
+++ Lab11.1A/src/com/javatunes/thread/MessagePrinterClient.java
@@ -9,7 +9,25 @@ package com.javatunes.thread;
 
 public class MessagePrinterClient {
 
-  public static void main(String[] args) {
-    // TODO: create two instances of your thread subclass and start them up
-  }
+    public static void main(String[] args) {
+        MessagePrinter m1 = new MessagePrinter("Roses are red");
+        MessagePrinter m2 = new MessagePrinter("Violets are blue");
+        MessagePrinter m3 = new MessagePrinter("I dont care");
+        MessagePrinter m4 = new MessagePrinter("and neither should you");
+
+        m1.setCount(10);
+        m1.setInterval(1500);
+        m2.setCount(20);
+        m2.setInterval(50);
+        m3.setCount(15);
+        m3.setInterval(1200);
+        m4.setCount(25);
+        m4.setInterval(10);
+
+        m1.start();
+        m2.start();
+        m3.start();
+        m4.start();
+        // create two instances of your thread subclass and start them up
+    }
 }
